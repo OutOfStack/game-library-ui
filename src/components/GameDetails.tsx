23da21ebbf2dc8a7bdcc4b04940451be93a87727
OutOfStack <ftavlad@gmail.com>
2024-01-20 00:31:51 +0300

    1

--- src/components/GameDetails.tsx
+++ src/components/GameDetails.tsx
@@ -174,12 +174,7 @@ const GameDetails = (props: IGameDetailsProps) => {
               {/* Close button, release date, publishers, developers ,genres, platforms, rating */}
               <Grid container xs={6} sm={6} md={7.5} direction="column" spacing={matchesXs ? 1 : 2}>
                 <Grid sx={{textAlign: 'right', pt: matchesXs ? 0 : theme.spacing(1), pb: 0}}>
-                  <IconButton
-                    sx={{padding: 0}}
-                    onClick={handleCloseDialog} 
-                    color="secondary" 
-                    size={matchesXs ? "small" : "medium"}
-                  >
+                  <IconButton onClick={handleCloseDialog} color="secondary" size={matchesXs ? "small" : "medium"} sx={{padding: 0}}>
                     <CloseIcon />
                   </IconButton>
                 </Grid>
