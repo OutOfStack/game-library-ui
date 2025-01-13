499f10aaeb7688fbe60cb8040d0e9fa2d69f0c71
Vladislav Zaramenskikh <ftavlad@gmail.com>
2025-01-14 00:16:51 +0300

    Use Vite instead of CRA (#35)

--- env.sh
+++ env.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # Recreate config file
-rm -rf ./env-config.js
-touch ./env-config.js
+rm -rf ./public/env-config.js
+touch ./public/env-config.js
 
-# Add assignment 
-echo "window._env_ = {" >> ./env-config.js
+# Add assignment
+echo "window._env_ = {" >> ./public/env-config.js
 
 # Read each line in .env file
-# Each line represents key=value pairs
-while read -r line || [[ -n "$line" ]];
-do
+while IFS= read -r line || [[ -n "$line" ]]; do
   # Split env variables by character `=`
   if printf '%s\n' "$line" | grep -q -e '='; then
     varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
@@ -18,12 +16,13 @@ do
   fi
 
   # Read value of current variable if exists as Environment variable
-  value=$(printf '%s\n' "${!varname}")
+  value=$(printf '%s' "${!varname}") # Use `printf` to avoid trailing newlines
   # Otherwise use value from .env file
-  [[ -z $value ]] && value=${varvalue}
+  [[ -z $value ]] && value=$(printf '%s' "$varvalue") # Trim and sanitize value
   
   # Append configuration property to JS file
-  echo "  $varname: \"$value\"," >> ./env-config.js
+  echo "  $varname: \"$value\"," >> ./public/env-config.js
 done < .env
 
-echo "}" >> ./env-config.js
+# Close the object
+echo "}" >> ./public/env-config.js
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    react()
+  ],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+  },
+  test: {
+    globals: true, // Use globals like `describe` and `test` without importing
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  }
+})
