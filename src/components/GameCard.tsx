6afe9c7456100927c8bb48e51d4ee41499187113
Vladislav Zaramenskikh <ftavlad@gmail.com>
2025-08-20 22:16:42 +0300

    Added sorting by rating and fixed image skeletons (#57)

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -84,6 +84,7 @@ const GameCard = (props: IGameCardProps) => {
           {!imageLoaded && (
             <Skeleton
               animation="wave"
+              variant="rectangular"
               sx={{ position: 'absolute', inset: 0, width: '100%', height: '100%', borderRadius: 0 }}
             />
           )}
